439efba16cd40f81da0a54fb3cc1a44002f2cb9f
Davi Scholl <davis@bandavide.com.br>
2021-11-15 17:23:40 -0300

    added first validation to model

--- app/controllers/market_lists_controller.rb
+++ app/controllers/market_lists_controller.rb
@@ -8,7 +8,9 @@ class MarketListsController < ApplicationController
   end
 
   def create
+    # raise params.require(:market_list).permit(:name, :market_date).inspect
     @market_list = MarketList.new(params.require(:market_list).permit(:name, :market_date))
     @market_list.save
+    render :new
   end
 end
